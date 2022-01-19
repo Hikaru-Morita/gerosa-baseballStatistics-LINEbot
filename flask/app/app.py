5460385009c54c036ef25b1cea8b7e0252bcaa89
Hikaru <ma20097@shibaura-it.ac.jp>
2022-01-19 15:35:03 +0900

    コネクションを生成し、登録済みかどうかを判定

--- flask/app/app.py
+++ flask/app/app.py
@@ -18,12 +18,13 @@ import numpy as np      # 数値計算
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://admin:admin@psql:5432/gerosa_linebot'
+SQLALCHEMY_DATABASE_URI = 'postgresql://admin:admin@psql:5432/gerosa_linebot'
+app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
 # 'postgresql://admin:admin@psql:5432/gerosa_linebot' は 'postgresql://[ユーザ名]:[パスワード]@[コンテナ名]:[ポート番号]/[データベース名]'
 
 gerosa_db = SQLAlchemy(app)
-gerosa_db.create_all()
-gerosa_db.session.commit()
+# gerosa_db.create_all()
+# gerosa_db.session.commit()
 
 # ファイルから取得
 # 後々環境変数に変更
@@ -31,6 +32,24 @@ json_file = open('./flask/app/secret.json', 'r')
 channel_json = json.load(json_file)
 line_bot_api = LineBotApi(channel_json['LINE_CHANNEL_ACCESS_TOKEN'])
 handler = WebhookHandler(channel_json['LINE_CHANNEL_SECRET'])
+
+def get_connection():
+    return psycopg2.connect(DATABASE_URL)
+
+
+def do_sql_select(sql):
+    result = None
+    with get_connection() as conn:
+        with conn.cursor() as cur:
+            cur.execute(sql)
+            result = cur.fetchall()
+    return result
+
+
+def do_sql_other(sql):
+    with get_connection() as conn:
+        with conn.cursor() as cur:
+            cur.execute(sql)
 
 # Line bot の webhook 用
 @app.route("/callback", methods=['POST'])
@@ -51,9 +70,33 @@ def callback():
 
     return 'OK'
 
-# メッセージをオウム返しにする
+
+
+# テキストメッセージを受信
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    user_id = str(event.source.user_id)
+    result = do_sql_select("SELECT * FROM gerosa-linebot WHERE userID='%s';" % user_id)
+
+    if result:  # データベースに登録されているユーザか判定
+        # 登録済み
+        line_bot_api.reply_message(
+            event.reply_token,
+            (
+                TextSendMessage('登録済み'),
+            )
+        )
+    else:
+        # 未登録
+        do_sql_other("INSERT INTO gerosa-linebot VALUES ('%s', '右', '右');" % user_id)
+        line_bot_api.reply_message(
+            event.reply_token,
+            (
+                TextSendMessage('未登録'),
+            )
+        )
+        # データベースに登録されていないユーザーは登録する
+
     line_bot_api.reply_message(
         event.reply_token,
         # TextSendMessage(text=event.message.text)
